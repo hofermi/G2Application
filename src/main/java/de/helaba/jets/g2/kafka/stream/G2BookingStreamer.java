e5831c663a59fae3163acf35702d8d309ccb7182
Benjamin Köhnen <ben@koehnen-home.de>
2021-05-19 13:58:20 +0200

    ongoing work

--- src/main/java/de/helaba/jets/g2/kafka/stream/G2BookingStreamer.java
+++ src/main/java/de/helaba/jets/g2/kafka/stream/G2BookingStreamer.java
@@ -1,5 +1,7 @@
 package de.helaba.jets.g2.kafka.stream;
 
+import de.helaba.jets.g2.kafka.avro.model.AvroG2BookingRecord;
+import de.helaba.jets.g2.kafka.avro.model.AvroMessageType;
 import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
 import java.util.Collections;
@@ -12,7 +14,11 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KGroupedStream;
+import org.apache.kafka.streams.kstream.KGroupedTable;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Named;
 import org.apache.kafka.streams.kstream.Produced;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -37,9 +43,30 @@ public class G2BookingStreamer {
         final Serde<GenericRecord> valueGenericAvroSerde = new GenericAvroSerde();
         valueGenericAvroSerde.configure(serdeConfig, false); // `false` for record values
 
-        final KStream<String, GenericRecord> kStream =
-                builder
-                        .stream(g2BookingTopicName, Consumed.with(Serdes.String(), valueGenericAvroSerde));
+        final KStream<String, GenericRecord> g2BookingRecords =
+                builder.stream(g2BookingTopicName, Consumed.with(Serdes.String(), valueGenericAvroSerde));
+
+        final KStream<String, GenericRecord> relevantG2BookingRecords =
+                g2BookingRecords
+                        .filter(
+                                (kopfnummer, record) -> {
+                                    AvroMessageType messageType = ((AvroG2BookingRecord) record).getMessageType();
+                                    switch (messageType) {
+                                        case PACS003:
+                                        case PACS008:
+                                            return true;
+                                        default:
+                                            return false;
+                                    }
+                                }
+                        );
+        //.map((kopfnummer, record) -> KeyValue.pair(kopfnummer, ((AvroG2BookingRecord) record).getAmount()));
+
+        final KGroupedStream<String, Long> g2BookingRecordsCounts =
+                relevantG2BookingRecords
+                        .toTable(Named.as("bookings"))
+                        .groupBy((kopfnummer, record) -> kopfnummer)
+                        .count(Named.as("no-of-bookings-per-kopfnummer"));
 
         //kStream
         //        .map((key, value) -> { // do something with each msg, square the values in our case
@@ -47,6 +74,21 @@ public class G2BookingStreamer {
         //            LOG.info(String.format("Received stream row with key %s: %s", key, value.toString()));
         //            return null;
         //        });
+
+        KStream<String, PlayEvent> playEvents =
+                builder.stream(Serdes.String(), playEventSerde, "play-events");
+
+        KStream<Long, PlayEvent> playsBySongId =
+                playEvents
+                        .filter((region, event) -> event.getDuration() >= MIN_CHARTABLE_DURATION)
+                        .map((key, value) -> KeyValue.pair(value.getSongId(), value));
+
+        KStream<Long, Song> songPlays =
+                playsBySongId.leftJoin(songTable, (playEvent, song) -> song, Serdes.Long(), playEventSerde);
+
+        KGroupedTable<Long, Long> groupedBySongId =
+                songPlays.groupBy((songId, song) -> songId, Serdes.Long(), Serdes.Long());
+        groupedBySongId.count(Named.as("song-play-count"));
 
         return kStream;
     }

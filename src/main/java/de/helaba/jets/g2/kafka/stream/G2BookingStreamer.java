ce589f78d0fce77125f6994c9f7d6f108c2dd762
Benjamin Köhnen <ben@koehnen-home.de>
2021-06-07 15:34:50 +0200

    ongoing work

--- src/main/java/de/helaba/jets/g2/kafka/stream/G2BookingStreamer.java
+++ src/main/java/de/helaba/jets/g2/kafka/stream/G2BookingStreamer.java
@@ -2,27 +2,15 @@ package de.helaba.jets.g2.kafka.stream;
 
 import de.helaba.jets.g2.kafka.avro.model.AvroG2BookingRecord;
 import de.helaba.jets.g2.kafka.avro.model.AvroMessageType;
-import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
-import java.util.Collections;
-import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.Named;
+import org.apache.kafka.streams.kstream.Predicate;
 import org.apache.kafka.streams.state.KeyValueStore;
-import org.apache.kafka.streams.state.StoreBuilder;
-import org.apache.kafka.streams.state.Stores;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.kafka.StreamsBuilderFactoryBeanCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -31,222 +19,68 @@ public class G2BookingStreamer {
 
     private static final Log LOG = LogFactory.getLog(G2BookingStreamer.class);
 
-    @Value(value = "${kafka.schemaRegistry.url}") // configured in application.properties
-    private String schemaRegistryUrl;
-
-    @Value(value = "${kafka.topic.g2Booking.name}") // configured in application.properties
-    private String g2BookingTopicName;
-
     @Value(value = "${kafka.stream.store.g2BookingsCounter}") // configured in application.properties
     private String g2BookingsCounterStoreName;
 
     @Bean
-    public java.util.function.Consumer<KStream<String, AvroG2BookingRecord>> newG2BookingStream() {
-        final Map<String, String> serdeConfig =
-                Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
-        final SpecificAvroSerde<AvroG2BookingRecord> g2BookingSerde = new SpecificAvroSerde<>();
-        g2BookingSerde.configure(serdeConfig, false); // `false` for record values
-
-        return input ->
-                input
-                        .filter(
-                                (kopfnummer, record) -> {
-                                    LOG.info(
-                                            String.format(
-                                                    "NEW Stream g2BookingRecords: Received record: %s",
-                                                    record.toString()
-                                            )
-                                    );
-                                    return true;
-                                }
-                        )
-                        .filter(
-                                (kopfnummer, record) -> {
-                                    AvroMessageType messageType = record.getMessageType();
-                                    switch (messageType) {
-                                        case PACS003:
-                                        case PACS008:
-                                            return true;
-                                        default:
-                                            return false;
-                                    }
-                                }
-                        )
-                        // filter is used for logging
-                        .filter(
-                                (kopfnummer, record) -> {
-                                    LOG.info(
-                                            String.format(
-                                                    "NEW Stream relevantG2BookingRecords: Received record: %s",
-                                                    record.toString()
-                                            )
-                                    );
-                                    return true;
-                                }
-                        )
-                        .groupBy((kopfnummer, record) -> kopfnummer, Grouped.with(Serdes.String(), g2BookingSerde))
-                        // write to key-value store
-                        .count(
-                                Materialized
-                                        .<String, Long, KeyValueStore<Bytes, byte[]>>as("newG2BookingsCounterStore")
-                                        .withKeySerde(Serdes.String())
-                                        .withValueSerde(Serdes.Long())
-                        );
-        //
-        //return input ->
-        //        input.foreach((key, value) -> {
-        //            System.out.println("Key: " + key + " Value: " + value);
-        //        });
-    }
-
-
-    //@Bean
-    //public StreamsBuilderFactoryBeanCustomizer customizer() {
-    //    return fb -> {
-    //        try {
-    //            final StreamsBuilder streamsBuilder = fb.getObject();
-    //            streamsBuilder.addGlobalStore(...);
-    //        }
-    //        catch (Exception e) {
-    //
-    //        }
-    //    };
-    //}
-
-    //@Bean("g2BookingStream")
-    //public KStream<String, AvroG2BookingRecord> createG2BookingStream(@Qualifier("oldG2BookingStreamBuilderFactory") StreamsBuilder builder) {
-    //    final Map<String, String> serdeConfig =
-    //            Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
-    //    final SpecificAvroSerde<AvroG2BookingRecord> g2BookingSerde = new SpecificAvroSerde<>();
-    //    g2BookingSerde.configure(serdeConfig, false); // `false` for record values
-    //
-    //    // all G2Bookings
-    //    final KStream<String, AvroG2BookingRecord> g2BookingRecords =
-    //            builder
-    //                    .stream(g2BookingTopicName, Consumed.with(Serdes.String(), g2BookingSerde))
-    //                    // filter is used for logging
-    //                    .filter(
-    //                            (kopfnummer, record) -> {
-    //                                LOG.info(
-    //                                        String.format(
-    //                                                "Stream g2BookingRecords: Received record: %s",
-    //                                                record.toString()
-    //                                        )
-    //                                );
-    //                                return true;
-    //                            }
-    //                    );
-    //
-    //    return g2BookingRecords;
-    //}
-
-    //@Bean("relevantG2BookingStream")
-    //public KStream<String, AvroG2BookingRecord> createRelevantG2BookingStream(@Qualifier("g2BookingStream") KStream<String, AvroG2BookingRecord> g2BookingRecords) {
-    //    // all G2Bookings for pacs.003 and pacs.008
-    //    final KStream<String, AvroG2BookingRecord> relevantG2BookingRecords =
-    //            g2BookingRecords
-    //                    .filter(
-    //                            (kopfnummer, record) -> {
-    //                                AvroMessageType messageType = record.getMessageType();
-    //                                switch (messageType) {
-    //                                    case PACS003:
-    //                                    case PACS008:
-    //                                        return true;
-    //                                    default:
-    //                                        return false;
-    //                                }
-    //                            }
-    //                    )
-    //                    // filter is used for logging
-    //                    .filter(
-    //                            (kopfnummer, record) -> {
-    //                                LOG.info(
-    //                                        String.format(
-    //                                                "Stream relevantG2BookingRecords: Received record: %s",
-    //                                                record.toString()
-    //                                        )
-    //                                );
-    //                                return true;
-    //                            }
-    //                    );
-    //
-    //    return relevantG2BookingRecords;
-    //}
-
-    //@Bean("groupedRelevantG2BookingStream")
-    //public KTable<String, Long> createG2BookingStream(@Qualifier("relevantG2BookingStream") KStream<String, AvroG2BookingRecord> relevantG2BookingRecords) {
-    //    final Map<String, String> serdeConfig =
-    //            Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
-    //    final SpecificAvroSerde<AvroG2BookingRecord> g2BookingSerde = new SpecificAvroSerde<>();
-    //    g2BookingSerde.configure(serdeConfig, false); // `false` for record values
-    //
-    //    final KTable<String, Long> g2BookingRecordsGrouped =
-    //            relevantG2BookingRecords
-    //                    .groupBy((kopfnummer, record) -> kopfnummer, Grouped.with(Serdes.String(), g2BookingSerde))
-    //                    // create a key-value store
-    //                    .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as(g2BookingsCounterStoreName)
-    //                                       .withKeySerde(Serdes.String())
-    //                                       .withValueSerde(Serdes.Long()));
-    //
-    //    return g2BookingRecordsGrouped;
-    //}
-
-    //@Bean
-    //public StoreBuilder g2BookingsCounterStore() {
-    //    return
-    //            Stores.keyValueStoreBuilder(
-    //                    Stores.persistentKeyValueStore(g2BookingsCounterStoreName),
-    //                    Serdes.String(),
-    //                    Serdes.Long()
-    //            );
-    //}
-
     /*
-    @KafkaStreamsStateStore(name="mystate", type= KafkaStreamsStateStoreProperties.StoreType.WINDOW, lengthMs=300000)
-    public void process(KStream<String, AvroG2BookingRecord> g2BookingRecords) {
-        final Map<String, String> serdeConfig =
-                Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
-        final SpecificAvroSerde<AvroG2BookingRecord> g2BookingSerde = new SpecificAvroSerde<>();
-        g2BookingSerde.configure(serdeConfig, false); // `false` for record values
-
-        // all G2Bookings for pacs.003 and pacs.008
-        final KStream<String, AvroG2BookingRecord> relevantG2BookingRecords =
-                g2BookingRecords
-                        .filter(
-                                (kopfnummer, record) -> {
-                                    AvroMessageType messageType = record.getMessageType();
-                                    switch (messageType) {
-                                        case PACS003:
-                                        case PACS008:
-                                            return true;
-                                        default:
-                                            return false;
-                                    }
-                                }
-                        )
-                        // filter is used for logging
-                        .filter(
-                                (kopfnummer, record) -> {
-                                    LOG.info(
-                                            String.format(
-                                                    "Stream relevantG2BookingRecords: Received record: %s",
-                                                    record.toString()
-                                            )
-                                    );
-                                    return true;
-                                }
-                        );
-
-        //KGroupedStream<String, String> groupedByWord = textLines
-        //        .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
-        //        .groupBy((key, word) -> word, Grouped.with(stringSerde, stringSerde));
-        final KGroupedStream<String, AvroG2BookingRecord> g2BookingRecordsGrouped =
-                relevantG2BookingRecords
-                        .groupBy((kopfnummer, record) -> kopfnummer, Grouped.with(Serdes.String(), g2BookingSerde));
-
-        // Create a key-value store
-        g2BookingRecordsGrouped.count(Materialized.as(g2BookingsCounterStoreName));
-    }
+     * Configured in application.properties:
+     * spring.cloud.stream.bindings.newG2BookingStream-in-0...
+     * spring.cloud.stream.kafka.streams.bindings.newG2BookingStream-in-0...
+     * spring.cloud.stream.kafka.streams.bindings.newG2BookingStream-in-0...
      */
+    public java.util.function.Consumer<KStream<String, AvroG2BookingRecord>> newG2BookingStream() {
+        return
+                input ->
+                        relevantG2BookingRecordsCount(
+                                input
+                                        // log received records
+                                        .filter(logMessagesPredicate("Received record: "))
+                                        // filter relevant message types
+                                        .filter(filterRelevantMsgTypesPredicate())
+                                        // log received relevant records
+                                        .filter(logMessagesPredicate("Received relevant record: "))
+                        );
+    }
+
+    private Predicate<String, AvroG2BookingRecord> logMessagesPredicate(String logPrefix) {
+        return
+                (kopfnummer, record) -> {
+                    LOG.info(logPrefix + record.toString());
+                    return true;
+                };
+    }
+
+    private Predicate<String, AvroG2BookingRecord> filterRelevantMsgTypesPredicate() {
+        return
+                (kopfnummer, record) -> {
+                    AvroMessageType messageType = record.getMessageType();
+                    switch (messageType) {
+                        case PACS003:
+                        case PACS008:
+                            return true;
+                        default:
+                            return false;
+                    }
+                };
+    }
+
+    private void relevantG2BookingRecordsCount(KStream<String, AvroG2BookingRecord> relevantG2BookingRecords) {
+        //final Map<String, String> serdeConfig =
+        //        Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+        //final SpecificAvroSerde<AvroG2BookingRecord> g2BookingSerde = new SpecificAvroSerde<>();
+        //g2BookingSerde.configure(serdeConfig, false); // `false` for record values
+
+        relevantG2BookingRecords
+                //.groupBy((kopfnummer, record) -> kopfnummer, Grouped.with(Serdes.String(), g2BookingSerde))
+                .groupByKey()
+                // write to key-value store => KTable<String, Long>
+                .count(
+                        Materialized
+                                .<String, Long, KeyValueStore<Bytes, byte[]>>as(g2BookingsCounterStoreName)
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(Serdes.Long())
+                );
+    }
 
 }
--- src/main/java/de/helaba/jets/g2/ui/MainView.java
+++ src/main/java/de/helaba/jets/g2/ui/MainView.java
@@ -4,28 +4,20 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import de.helaba.jets.g2.kafka.avro.model.AvroG2BookingRecord;
 import de.helaba.jets.g2.kafka.event.G2BookingPayload;
 import de.helaba.jets.g2.kafka.event.G2BookingPayloadUtil;
 import de.helaba.jets.g2.kafka.producer.G2BookingProducer;
-import de.helaba.jets.g2.kafka.stream.G2BookingStreamer;
 import java.time.Duration;
 import java.time.Instant;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.state.HostInfo;
 import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.QueryableStoreTypes;
 import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
-import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 
 /**
  * access main view via:
@@ -41,33 +33,8 @@ public class MainView extends VerticalLayout {
     @Autowired
     private G2BookingProducer g2BookingProducer;
 
-    //@Autowired
-    //private G2BookingStreamer g2BookingStreamer;
-
-    //@Autowired
-    //private KStream<String, AvroG2BookingRecord> g2BookingStream;
-
     @Autowired
     private InteractiveQueryService interactiveQueryService;
-
-    //@Autowired
-    //@Qualifier("oldG2BookingStreamBuilderFactory")
-    //private StreamsBuilderFactoryBean oldG2BookingStreamBuilderFactory;
-
-    //@Autowired
-    //private KTable<String, Long> groupedRelevantG2BookingStream;
-
-    //@Autowired
-    //private KafkaStreamsRegistry kafkaStreamsRegistry;
-
-    //@Autowired
-    //private KafkaStreamsBinderConfigurationProperties binderConfigurationProperties;
-
-    //@Autowired
-    //private KafkaStreams kafkaStreams;
-
-    //@Autowired
-    //private ProcessorContext processorContext;
 
     @Value(value = "${kafka.stream.store.g2BookingsCounter}") // configured in application.properties
     private String g2BookingsCounterStoreName;
@@ -98,74 +65,16 @@ public class MainView extends VerticalLayout {
     }
 
     private void printStreamStateStores() {
-        //LOG.info("Store name: " + groupedRelevantG2BookingStream.queryableStoreName());
-        //StateStore stateStore = processorContext.getStateStore(g2BookingsCounterStoreName);
-        //LOG.info("State store: " + stateStore.name());
-        /*
-        groupedRelevantG2BookingStream
-                .toStream()
-                // filter is used for logging
-                .filter(
-                      (kopfnummer, count) -> {
-                          LOG.info(
-                                  String.format(
-                                          "KTable groupedRelevantG2BookingStream: (%s, %d)",
-                                          kopfnummer,
-                                          count
-                                  )
-                          );
-                          return true;
-                      }
-                );
-         */
-
-        //HostInfo hostInfo = interactiveQueryService.getCurrentHostInfo();
-        //LOG.info("Host: " + hostInfo);
-
-
-        //kafkaStreamsRegistry.getKafkaStreams().forEach((k) -> {
-        //    KeyQueryMetadata keyQueryMetadata = k.queryMetadataForKey(store, key, serializer);
-        //    if (keyQueryMetadata != null) {
-        //        kafkaStreamsAtomicReference.set(k);
-        //    }
-        //
-        //});
-
-        //KafkaStreams kafkaStreams = oldG2BookingStreamBuilderFactory.getKafkaStreams();
-        //System.out.println("State: " + kafkaStreams.state());
-        //kafkaStreams.store("newG2BookingsCounterStore", QueryableStoreTypes.keyValueStore());
-
-        //ReadOnlyKeyValueStore<String, Long> keyValueStore =
-        ReadOnlyKeyValueStore<Object, Object> keyValueStore =
+        ReadOnlyKeyValueStore<String, Long> keyValueStore =
                 interactiveQueryService.getQueryableStore(
-                        "newG2BookingsCounterStore",
+                        g2BookingsCounterStoreName,
                         QueryableStoreTypes.keyValueStore()
                 );
 
-        //LOG.info(String.format("%s", keyValueStore.get("2020")));
-
-        for (KeyValueIterator<Object, Object> it = keyValueStore.all(); it.hasNext(); ) {
-            KeyValue<Object, Object> entry = it.next();
+        for (KeyValueIterator<String, Long> it = keyValueStore.all(); it.hasNext(); ) {
+            KeyValue<String, Long> entry = it.next();
             LOG.info(String.format("%s: %s", entry.key, entry.value));
         }
-
-        /*
-        // Get the key-value store CountsKeyValueStore
-        ReadOnlyKeyValueStore<String, Long> keyValueStore =
-                streams.store("CountsKeyValueStore", QueryableStoreTypes.keyValueStore());
-
-        // Get value by key
-        System.out.println("count for hello:" + keyValueStore.get("hello"));
-
-        // Get the values for a range of keys available in this application instance
-        KeyValueIterator<String, Long> range = keyValueStore.range("all", "streams");
-        while (range.hasNext()) {
-            KeyValue<String, Long> next = range.next();
-            System.out.println("count for " + next.key + ": " + next.value);
-        }
-        // close the iterator to release resources
-        range.close();
-         */
     }
 
 }
